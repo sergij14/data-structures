f7f0bf88a08f57d467ebc6f85b6fbb3e6735a6e8
sergij14 <sergi.jajanidze@gmail.com>
2022-10-08 09:01:11 +0400

    created weave function for combining two queues

--- src/data-structures/queue.ts
+++ src/data-structures/queue.ts
@@ -1,5 +1,7 @@
 // Queue
 
+import { win32 } from "path";
+
 // FIFO - first in first out
 // creating an an array with some methods
 // unshift - adding in the beginning,
@@ -23,4 +25,25 @@ export class Queue {
   peek() {
     return this.data[this.data.length - 1];
   }
+}
+
+// Weave
+// receives two queues as arguments and combines the
+// contents of each into a new, third queue.
+// new queue contains the *alterating* content
+// of the two queues.
+
+export function weave(q1: Queue, q2: Queue) {
+  const q = new Queue();
+
+  while (q1.peek() || q2.peek()) {
+    if (q1.peek()) {
+      q.add(q1.remove());
+    }
+    if (q2.peek()) {
+      q.add(q2.remove());
+    }
+  }
+
+  return q;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('log')
+import { Queue, weave } from "./data-structures/queue";
+
+const q1 = new Queue();
+q1.add("1");
+q1.add("2");
+
+const q2 = new Queue();
+q1.add(1);
+q1.add(2);
+
+const q = weave(q1, q2);
+
+console.log(q);
