ae64a05da690db4386b0e76ddde1b313de76fe01
sergij14 <sergi.jajanidze@gmail.com>
2022-10-09 13:31:48 +0400

    implemented bubbleSort and linearSerach alogirthms

---
+++ src/algorithms/linearSearch.ts
@@ -0,0 +1,15 @@
+// Linear Search
+// checks every element in the array, first time sees value, returns it
+// works on sorted and unsorted lists
+
+// 0(n) - in the worst case we have to visit all of the items
+export function linearSearch(arr: number[], el: number) {
+  for (let idx = 0; idx < arr.length; idx++) {
+    if (arr[idx] == el) {
+      return idx;
+    }
+    return false;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////
